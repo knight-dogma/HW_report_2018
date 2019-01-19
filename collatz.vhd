0137d6ac484ffddfb51dcc51ec3799c0857d9a79
Koki Ryu <liukoki@gmail.com>
2019-01-19 22:50:03 +0900

    Add barrel shifter : v3

--- collatz.vhd
+++ collatz.vhd
@@ -21,6 +21,8 @@ architecture Collatz_Body of Collatz is
     variable tmp_place : std_logic_vector(17 downto 0) := (others => '0');
     variable tmp_peak : std_logic_vector(17 downto 0) := (others => '0');
     variable TMP_LEN : std_logic_vector(7 downto 0) := (others => '0');
+
+    variable shift : std_logic_vector(4 downto 0) := (others => '0');
   begin
     if RESET = '1' then
       TMP_PLACE := "00000000" & START;
@@ -31,19 +33,38 @@ architecture Collatz_Body of Collatz is
       if TMP_PLACE = "000000000000000001" then
         FIN <= '1';
       else
-        if TMP_PLACE(0) = '0' then
-          TMP_PLACE := '0' & TMP_PLACE(17 downto 1);
-          TMP_LEN := TMP_LEN + "00000001";
-        else
+        if TMP_PLACE(0) = '1' then
           TMP_PLACE := std_logic_vector(to_unsigned(to_integer(unsigned( TMP_PLACE * three )) + 1, 18));
           if TMP_PLACE > TMP_PEAK then
             TMP_PEAK := TMP_PLACE;
           end if;
-          TMP_PLACE := '0' & TMP_PLACE(17 downto 1);
+          TMP_LEN := TMP_LEN + "00000001";
+        end if;
+        for i in 17 downto 0 loop
+          if TMP_PLACE(i) = '1' then
+            shift := std_logic_vector(to_unsigned(i, 5));
+          end if;
+        end loop;
+        if shift(4) = '1' then
+          TMP_PLACE := "0000000000000000" & TMP_PLACE(17 downto 16);
+          TMP_LEN := TMP_LEN + "00010000";
+        end if;
+        if shift(3) = '1' then
+          TMP_PLACE := "00000000" & TMP_PLACE(17 downto 8);
+          TMP_LEN := TMP_LEN + "00001000";
+        end if;
+        if shift(2) = '1' then
+          TMP_PLACE := "0000" & TMP_PLACE(17 downto 4);
+          TMP_LEN := TMP_LEN + "00000100";
+        end if;
+        if shift(1) = '1' then
+          TMP_PLACE := "00" & TMP_PLACE(17 downto 2);
           TMP_LEN := TMP_LEN + "00000010";
         end if;
-
-
+        if shift(0) = '1' then
+          TMP_PLACE := "0" & TMP_PLACE(17 downto 1);
+          TMP_LEN := TMP_LEN + "00000001";
+        end if;
       end if;
     end if;
     PEAK <= TMP_PEAK;
