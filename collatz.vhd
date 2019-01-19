0f34539fc803cb4b2c63692738342549f999fe4d
Koki Ryu <liukoki@gmail.com>
2019-01-19 22:27:20 +0900

    implement runaway technique

--- collatz.vhd
+++ collatz.vhd
@@ -15,33 +15,35 @@ entity Collatz is
 end Collatz;
 
 architecture Collatz_Body of Collatz is
-  SIGNAL TMP_PLACE : std_logic_vector(17 downto 0) := (others => '0');
-  SIGNAL TMP_PEAK : std_logic_vector(17 downto 0) := (others => '0');
-  SIGNAL TMP_LEN : std_logic_vector(7 downto 0) := (others => '0');
   constant three : std_logic_vector(17 downto 0) := "000000000000000011";
   begin
   Climb : process (CLK, RESET, START)
+    variable tmp_place : std_logic_vector(17 downto 0) := (others => '0');
+    variable tmp_peak : std_logic_vector(17 downto 0) := (others => '0');
+    variable TMP_LEN : std_logic_vector(7 downto 0) := (others => '0');
   begin
     if RESET = '1' then
-      TMP_PLACE <= "00000000" & START;
-      TMP_PEAK <= "00000000" & START;
-      TMP_LEN <= "00000000";
+      TMP_PLACE := "00000000" & START;
+      TMP_PEAK := "00000000" & START;
+      TMP_LEN := "00000000";
       FIN <= '0';
     elsif CLK'event and CLK = '1' then
       if TMP_PLACE = "000000000000000001" then
         FIN <= '1';
       else
         if TMP_PLACE(0) = '0' then
-          TMP_PLACE <= '0' & TMP_PLACE(17 downto 1);
+          TMP_PLACE := '0' & TMP_PLACE(17 downto 1);
+          TMP_LEN := TMP_LEN + "00000001";
         else
-          TMP_PLACE <= std_logic_vector(to_unsigned(to_integer(unsigned( TMP_PLACE * three )) + 1, 18));
+          TMP_PLACE := std_logic_vector(to_unsigned(to_integer(unsigned( TMP_PLACE * three )) + 1, 18));
+          if TMP_PLACE > TMP_PEAK then
+            TMP_PEAK := TMP_PLACE;
+          end if;
+          TMP_PLACE := '0' & TMP_PLACE(17 downto 1);
+          TMP_LEN := TMP_LEN + "00000010";
         end if;
 
-        if TMP_PLACE > TMP_PEAK then
-          TMP_PEAK <= TMP_PLACE;
-        end if;
 
-        TMP_LEN <= TMP_LEN + "00000001";
       end if;
     end if;
     PEAK <= TMP_PEAK;
